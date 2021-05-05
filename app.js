e41d8a0a51d2cbd244f47d9c1d5a743ea76686f9
henok <henosmak55@gmail.com>
2021-05-04 22:16:03 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('selam BlackBeard'));
+app.get('/', (req, res) => res.send('ሰለሜ ሰለሜ'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
